738b62c83fc8b81b5ff3c2f4f225b156c9093d27
Mario Norambuena Espinosa <ma.norambuenae@duocuc.cl>
2024-09-03 19:58:42 -0400

    creación de navbar y testeo de color

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import Loginup from './login';
 import CartShopping from "./cart";
 import Header from "./header";
+import NavBar from "./navBar";
 import Footer from "./footer";
 import './styles/Home.css';
 function Home(){
     return(
         <>
-        <Header></Header>
-        <h1 className="Home">Este es el componente casa</h1>
+        <body>
+            <Header></Header>
+            <h1 className="Home">Este es el componente casa</h1>
 
-        <Loginup></Loginup>
+            <Loginup></Loginup>
 
-        <CartShopping></CartShopping>
-        <Footer></Footer>
+            <CartShopping></CartShopping>
+            <Footer></Footer>
+        </body>
         </>
     );
 }
---
+++ src/components/navBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './styles/navbar.css'
+function NavBar(){
+    return(
+        <h1></h1>
+    );
+}
+
+export default NavBar
