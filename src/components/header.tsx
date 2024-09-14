c8e02dd24748f3dab1a5d104dd4b22fb29e3cafe
Jordan Flores Pereira <Jord.floresp@duocuc.cl>
2024-09-14 10:29:51 -0300

    Menu desplegable p1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,39 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './styles/header.css'
+import React, { useState } from "react";
+import './styles/navbar.css';
 
-function Header(){
-    return(
-        <>
-        <header className="Header">
-            <Link to="/">
-            <button className="Inicio">
-                <p>Inicio</p>
-            </button></Link>
-            <Link to="/Categorias">
-            <button>
-                <p>Categorias</p>
-            </button>
-            </Link>
-            <Link to ="/Locales">
-            <button>
-                <p>Locales</p>
-            </button>
-            </Link>
-            <Link to="/Perfil">
-            <button>
-                <p>Perfil</p>
-            </button>
-            </Link>
-            <Link to="/Carrito">
-            <button>
-                <p>Carrito</p>
-            </button>
-            </Link>
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <header className="navbar">
+            <div className="nav_logo">Logo</div>
+            <div className={`nav_item ${isOpen ? "open" : ""}`}>
+                <a href="#">INICIO</a>
+                <a href="#">CATEGORIAS</a>
+                <a href="#">LOCALES</a>
+                <a href="#">PERFIL</a>
+                <a href="#">CARRITOS</a>
+            </div>
+            <div className={`nav_toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </header>
-        </>
-
     );
-}
+};
 
-export default Header
+export default Header;
+
+<>
+{/* <header className="Header">
+    <Link to="/">
+    <button className="Inicio">
+        <p>Inicio</p>
+    </button></Link>
+    <Link to="/Categorias">
+    <button>
+        <p>Categorias</p>
+    </button>
+    </Link>
+    <Link to ="/Locales">
+    <button>
+        <p>Locales</p>
+    </button>
+    </Link>
+    <Link to="/Perfil">
+    <button>
+        <p>Perfil</p>
+    </button>
+    </Link>
+    <Link to="/Carrito">
+    <button>
+        <p>Carrito</p>
+    </button>
+    </Link>
+</header> */}
+</>
+
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles/navbar.css'
-function NavBar(){
-    return(
-        <h1></h1>
+
+const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    
+    return (
+        <div className="navbar">
+            <div className="nav_logo">CaballeroMarket</div>
+            <div className={`nav_item ${isOpen ? "open" : ""}`}>
+                <a href="#">INICIO</a>
+                <a href="#">CATEGORIAS</a>
+                <a href="#">LOCALES</a>
+                <a href="#">PERFIL</a>
+                <a href="#">CARRITOS</a>
+            </div>
+            <div className={`nav_toggle ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     );
 }
 
-export default NavBar
+export default NavBar;
+
+
+<>
+{/* <header className="Header">
+    <Link to="/">
+    <button className="Inicio">
+        <p>Inicio</p>
+    </button></Link>
+    <Link to="/Categorias">
+    <button>
+        <p>Categorias</p>
+    </button>
+    </Link>
+    <Link to ="/Locales">
+    <button>
+        <p>Locales</p>
+    </button>
+    </Link>
+    <Link to="/Perfil">
+    <button>
+        <p>Perfil</p>
+    </button>
+    </Link>
+    <Link to="/Carrito">
+    <button>
+        <p>Carrito</p>
+    </button>
+    </Link>
+</header> */}
+</>
