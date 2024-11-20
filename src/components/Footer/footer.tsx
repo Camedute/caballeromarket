897a8f51c171feb32e48a29e72cbd50138517b2b
Jordan Flores Pereira <Jord.floresp@duocuc.cl>
2024-11-20 13:52:02 -0300

    mejoras footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import { Link } from "react-router-dom"; // Importamos Link para la navegación interna
 import './footer.css';
 
 const Footer: React.FC = () => {
@@ -9,21 +11,21 @@ const Footer: React.FC = () => {
           <h3>Caballero Market</h3>
           <p>Tu mejor mercado en línea. Encuentra productos de calidad a los mejores precios.</p>
         </div>
-        <div className="footer_section links">
-          <h3>Enlaces Rápidos</h3>
-          <ul>
-            <li><a href="/about">Sobre Nosotros</a></li>
-            <li><a href="/contact">Contacto</a></li>
-            <li><a href="/privacy">Política de Privacidad</a></li>
-            <li><a href="/terms">Términos de Servicio</a></li>
-          </ul>
-        </div>
         <div className="footer_section social">
           <h3>Síguenos</h3>
           <ul>
-            <li><a href="https://facebook.com">Facebook</a></li>
-            <li><a href="https://twitter.com">Twitter</a></li>
-            <li><a href="https://instagram.com">Instagram</a></li>
+            <li><a href="https://facebook.com" aria-label="Facebook"><FaFacebook /></a></li>
+            <li><a href="https://twitter.com" aria-label="Twitter"><FaTwitter /></a></li>
+            <li><a href="https://instagram.com" aria-label="Instagram"><FaInstagram /></a></li>
+          </ul>
+        </div>
+        <div className="footer_section links">
+          <h3>Enlaces Rápidos</h3>
+          <ul>
+            <li><Link to="/home">Inicio</Link></li>
+            <li><Link to="/perfil">Perfil</Link></li>
+            <li><Link to="/inventario">Inventario</Link></li>
+            <li><Link to="/carrito">Pedidos</Link></li>
           </ul>
         </div>
       </div>
@@ -32,6 +34,6 @@ const Footer: React.FC = () => {
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
